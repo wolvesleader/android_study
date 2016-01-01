234108893342f72e0bf4b80d4f8ecbc5d8f8e287
miaoqing <miaoqing2014@163.com>
2016-01-01 15:39:51 +0800

    微博分享官方的demo
    对官方的demo做了一些简化

---
+++ WeiboSDKDemo/src/com/sina/weibo/sdk/demo/MyTextView.java
@@ -0,0 +1,22 @@
+package com.sina.weibo.sdk.demo;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class MyTextView extends TextView{
+
+	public MyTextView(Context context) {
+		super(context);
+		setFocusable(true);
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	
+	
+
+}
